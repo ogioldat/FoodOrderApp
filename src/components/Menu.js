6060c61d1d9a83842264c9e5fbd84839f7d6cfad
Tomeczqueee <0faighter123@gmail.com>
2019-10-02 17:37:11 +0200

    'hmm'

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,28 +1,56 @@
 import React, {Component} from 'react';
 import { stack as Menu } from 'react-burger-menu'
 import '../assets/styles/menu.css'
+import HamburgerMenu from "react-hamburger-menu";
+import SlideMenu from 'react-slide-menu'
+
 
 class MenuNav extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            menuOpen:false
-        }
+            open: false,
+            menuClicked: false,
+            slideMenuActive: true,
+        };
     }
 
+
     render() {
+        const nav = [
+            {id: 'home', label: 'Home', path: '/'},
+            {id: 'about', label: 'About', path: '/about'},
+            {id: 'discover', label: 'Discover', path: '/discover'},
+        ];
+
         return (
             <>
-                <Menu
-                    isOpen={this.state.menuOpen}
-                >
-                    <p>Twój stary</p>
-                    <p>Twój stary</p>
-                    <p>Twój stary</p>
-                </Menu>
+                {/*<Menu*/}
+                {/*    customBurgerIcon={<HamburgerMenu/>}*/}
+                {/*    customCrossIcon={<HamburgerMenu*/}
+                {/*        isOpen={true}*/}
+                {/*        color={'white'}*/}
+                {/*    />}*/}
+                {/*    isOpen={this.state.isOpen}*/}
+                {/*>*/}
+                {/*    <p>Twój stary</p>*/}
+                {/*    <p>Twój stary</p>*/}
+                {/*    <p>Twój stary</p>*/}
+                {/*</Menu>*/}
+
+                <SlideMenu
+                    active={this.state.slideMenuActive}
+                    nav={nav}
+                    reactRouter={false}
+                    closeMenu={() => this.setState({slideMenuActive: false})}>
+                    <div>
+                        Sth be here
+                    </div>
+                </SlideMenu>
             </>
         );
     }
 }
+
 
 export default MenuNav;
